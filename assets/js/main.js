40e7ffb2f5b2470ac4a7c8e45afa3c521b915ae5
karim12345 <k.abdelmegeed@nouracademy.com>
2020-07-06 22:58:16 +0200

    add missing files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Appland - v2.2.0
-* Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
@@ -132,51 +127,10 @@
     return false;
   });
 
-  // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center: true,
-    margin: 25,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      },
-      1200: {
-        items: 5
-      }
-    }
-  });
-
-  // Initiate venobox lightbox
   $(document).ready(function() {
     $('.venobox').venobox();
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      900: {
-        items: 2
-      }
-    }
-  });
 
   // Init AOS
   function aos_init() {
